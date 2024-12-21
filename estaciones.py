8126f0247ca14b106d3c6fe4e1e24bbd89b5d9c6
geonet-geomatica <geonet.geomatica@gmail.com>
2024-12-21 14:07:28 -0300

    Update estaciones.py

--- estaciones.py
+++ estaciones.py
@@ -8,12 +8,9 @@ logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %
 
 BASE_URL = "http://meteorologia.onrender.com/wfs"
 
-# Función para escapar caracteres especiales
+# Función para escapar caracteres XML
 def escape_xml(value):
-    if value is None:
-        return ""
-    # Escapar caracteres especiales específicos de XML
-    return escape(str(value))
+    return escape(str(value)) if value is not None else ""
 
 @app.route('/wfs', methods=['GET'])
 def wfs_service():
@@ -59,46 +56,4 @@ def wfs_service():
             <GetFeature>
                 <DCPType>
                     <HTTP>
-                        <Get xlink:href="{BASE_URL}?SERVICE=WFS&amp;REQUEST=GetFeature"/>
-                    </HTTP>
-                </DCPType>
-            </GetFeature>
-        </Request>
-    </Capability>
-    <FeatureTypeList>
-        <FeatureType>
-            <Name>{escape_xml(typename)}</Name>
-            <Title>Estaciones Agroclimáticas</Title>
-            <Abstract>Estaciones meteorológicas de Mendoza</Abstract>
-            <DefaultSRS>{escape_xml(srsname)}</DefaultSRS>
-            <LatLongBoundingBox minx="-70.0" miny="-35.0" maxx="-68.0" maxy="-32.0"/>
-        </FeatureType>
-    </FeatureTypeList>
-</wfs:WFS_Capabilities>"""
-            return Response(capabilities_xml, content_type="text/xml")
-
-        elif request_type and request_type.upper() == "GETFEATURE":
-            gml_response = f"""<?xml version="1.0" encoding="UTF-8"?>
-<wfs:FeatureCollection xmlns:wfs="http://www.opengis.net/wfs"
-                       xmlns:gml="http://www.opengis.net/gml"
-                       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                       xsi:schemaLocation="{BASE_URL} {BASE_URL}?SERVICE=WFS&amp;REQUEST=DescribeFeatureType">
-    <gml:featureMember>
-        <Estaciones>
-            <Nombre>{escape_xml("Estación 1")}</Nombre>
-            <Fecha>{escape_xml("2024-01-01")}</Fecha>
-            <Temperatura_Aire>{escape_xml("20°C")}</Temperatura_Aire>
-            <Humedad>{escape_xml("50%")}</Humedad>
-            <Punto_de_Rocío>{escape_xml("10°C")}</Punto_de_Rocío>
-            <Velocidad_Viento>{escape_xml("5 m/s")}</Velocidad_Viento>
-            <Dirección_del_Viento>{escape_xml("NE")}</Dirección_del_Viento>
-        </Estaciones>
-    </gml:featureMember>
-</wfs:FeatureCollection>"""
-            return Response(gml_response, content_type="text/xml")
-
-    return Response("Solicitud WFS no válida.", status=400)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+                        <Get xlink:href="{BASE_URL}?SERVICE=WFS&amp;REQUEST=GetFeature"/
